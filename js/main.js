77bc0770b5993958037a7052a64b72eb2d0d3334
Usubjonovshuxratbek <usubjonovshuxratbek1@gmail.com>
2023-05-10 19:22:57 +0300

    ginish

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let elBtn = document.querySelector('.btn-menu')
+let elNav = document.querySelector('.nav')
+let elNavInner = document.querySelector('.nav__inner')
+let elNavInnerLink = document.querySelector('.nav__inner-link')
+elNav.classList.add()
+elBtn.addEventListener('click', ()=>{
+    elNav.classList.toggle('navv')
+})
